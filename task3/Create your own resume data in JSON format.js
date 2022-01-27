5b29ebc8265b6ecef7b6ff2bc9e5e24e2188d3cc
shanmugams425 <46421163+shanmugams425@users.noreply.github.com>
2022-01-27 16:44:40 +0530

    Add files via upload

---
+++ task3/Create your own resume data in JSON format.js
@@ -0,0 +1,13 @@
+var resume=[{"Name":"shanmugam",
+             "qualification":"B.E",
+             "course":"mechanical engineering",
+             "email":"shan@gmail.com",
+            "Experience":"fresher",
+        "Address":"abskmfsl",
+    "Domain":"Full stack development",}];
+ 
+resume.forEach(function() {
+    console.log(resume);
+});
+  
+let b = JSON.parse(resume)
